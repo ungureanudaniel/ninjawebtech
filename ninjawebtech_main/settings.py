93f94d338042841a3e077b76467c5341f9f93fbf
Daniel Ungureanu <ungureanu.daniel86@gmail.com>
2021-07-05 23:57:19 +0300

    Static files settings

--- ninjawebtech_main/settings.py
+++ ninjawebtech_main/settings.py
@@ -120,10 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT  =   os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static'),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 #----------------MEDIA FILES STORAGE SETTINGS--------------------------
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
