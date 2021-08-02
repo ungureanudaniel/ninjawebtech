4df57b1bfff4298cc3b60ff8ee08bc496c522c6f
Daniel Ungureanu <ungureanu.daniel86@gmail.com>
2021-08-02 23:01:28 +0300

    Set up whitenoise to serve static files in production

--- ninjawebtech_main/settings.py
+++ ninjawebtech_main/settings.py
@@ -23,6 +23,7 @@ ALLOWED_HOSTS = ['127.0.0.1', 'https://www.ninjaweb.tech', 'ninjaweb.tech', 'www
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -43,6 +44,7 @@ SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -128,6 +130,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
