b234b9cb89249c6087699ad13b8983a562446a9a
Daniel Ungureanu <ungureanu.daniel86@gmail.com>
2021-08-05 23:10:50 +0300

    Testimonials slick slider with css and some js

--- static/js/master.js
+++ static/js/master.js
@@ -10,6 +10,7 @@
 			mainSlider();
 			ContentSlider();
 			testimonialSlider();
+			NoUnstyledContent();
 			// testimonialSliderTwo();
 			clientSlider();
 			blogGridSlider();
@@ -153,61 +154,28 @@
 
 	/* ~~~ Testimonials Slider ~~~ */
 	function testimonialSlider(){
-	$(".testimonial").slick({
-        dots: true,
-        infinite: true,
-        centerMode: true,
-        slidesToShow: 3,
-        slidesToScroll: 3,
-		centerPadding: '0',
-		responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-  ]
+		$('.testimonial').slick({
+			dots: true,
+	    infinite: true,
+	    centerMode: true,
+	    slidesToShow: 1,
+			infinite: true,
+		  autoplay: true,
+		  autoplaySpeed: 6000,
+		  speed: 800,
+		  slidesToShow: 1,
+		  adaptiveHeight: true,
+			
 	});
-	}
+}
 
-	/* ~~~ Testimonial Slider 2 ~~~ */
-	// function testimonialSliderTwo(){
-	// $(".testimonial-style-2").slick({
-  //       dots: true,
-  //       infinite: true,
-  //       centerMode: true,
-	// 			autoplay: true,
-  //       autoplaySpeed: 7000,
-  //       slidesToShow: 1,
-  //       slidesToScroll: 1,
-	// 			centerPadding: '0',
-	// 			responsive: [
-	// 	    	{
-	// 			  breakpoint: 480,
-	// 			  settings: {
-	// 				arrows: false,
-	// 			  }
-	// 			}
-  // 		]
-  //     });
-	// }
+function NoUnstyledContent(){
+	$( document ).ready(function() {
+	$('.no-fouc').removeClass('no-fouc');
+	});
+}
+
+
 
 	/* ~~~ Client Slider ~~~ */
 	function clientSlider() {
